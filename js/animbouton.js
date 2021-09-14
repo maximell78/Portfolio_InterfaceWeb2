4ffc701b2e97010c23a6431679e57483534c9736
maximell78 <maximell78@users.noreply.github.com>
2021-09-13 21:14:48 -0400

    update 13/09/2021
    Construction de la page du site HTML  et du site PHP.

---
+++ js/animbouton.js
@@ -0,0 +1,40 @@
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+
+// Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+/**
+* Event handler for beforeinstallprompt event.
+*   Saves the event & shows install button.
+*
+* @param {Event} evt
+*/
+function saveBeforeInstallPromptEvent(evt) {
+// CODELAB: Add code to save event & show the install button.
+deferredInstallPrompt = evt;
+installButton.removeAttribute('hidden');
+}
+/**
+* Event handler for butInstall - Does the PWA installation.
+*
+* @param {Event} evt
+*/
+function installPWA(evt) {
+    // Add code show install prompt & hide the install button.
+    deferredInstallPrompt.prompt();
+    // Hide the install button, it can't be called twice.
+    evt.srcElement.setAttribute('hidden', true);
+}
+// Log user response to prompt.
+
+deferredInstallPrompt.userChoice
+    .then((choice) => {
+        if (choice.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt', choice);
+        } else {
+            console.log('User dismissed the A2HS prompt', choice);
+        }
+        deferredInstallPrompt = null;
+    });
