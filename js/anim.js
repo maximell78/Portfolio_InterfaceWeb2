839255340038edc7900f0b11235801dc1a6d78e0
maximell78 <maximell78@users.noreply.github.com>
2021-09-18 00:46:50 -0400

    update

--- js/anim.js
+++ js/anim.js
@@ -1,16 +1,16 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml16');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var textWrapper = document.querySelector('.ml__16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter__16'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml16 .letter',
+    targets: '.ml__16 .letter__16',
     translateY: [-100,0],
     easing: "easeOutExpo",
     duration: 1400,
     delay: (el, i) => 30 * i
   }).add({
-    targets: '.ml16',
+    targets: '.ml__16',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v8';
+const CACHE_NAME = 'static-cache-v8.1';
 
 self.addEventListener('activate', (evt) => {
 console.log('[ServiceWorker] Activate');
@@ -55,7 +55,7 @@ self.addEventListener('fetch', (evt) => {
             .catch(() => {
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
-                        return cache.match('Portfolio_InterfaceWeb2/offline.html');
+                        return cache.match('https://maximell78.github.io/Portfolio_InterfaceWeb2/offline.html');
                     });
             })
     );
