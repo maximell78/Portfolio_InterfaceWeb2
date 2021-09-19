fb09f8243a44e230d51f96d231b60a0893088624
maximell78 <maximell78@users.noreply.github.com>
2021-09-18 23:49:22 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v25';
+const CACHE_NAME = 'static-cache-v26';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
